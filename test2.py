e27fc939cba0fb3423e61ac2bf39db83e62fd935
Biren Shah <biren@birenshah.com>
2013-09-08 21:00:13 -0700

    Removed unneeded config.py and test.py

--- test2.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-cnx = mysql.connector.connect(user='biren', password='cArrion69',
-                              host='birenshah.ciic9oqhvrvk.us-east-1.rds.amazonaws.com',
-                              port='3306',
-                              database='scribblevault')
-cnx.close()
