a4d70a829998e3d069a813216f09045dffdaca31
Biren Shah <biren@birenshah.com>
2013-09-08 20:59:40 -0700

    Updated main.py with new admin functions, removed parse import

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ import logging
 
 tpath = os.path.join(os.path.dirname(__file__), 'static/templates/')
 
+# Some mobile browsers which look like desktop browsers.
+RE_MOBILE = re.compile(r"(iphone|ipod|blackberry|android|palm|windows\s+ce)", re.I)
+RE_DESKTOP = re.compile(r"(windows|linux|os\s+[x9]|solaris|bsd)", re.I)
+RE_BOT = re.compile(r"(spider|crawl|slurp|bot)", re.I)
+
 def getUserAndSetupStream(request, response):
 	gUser = users.get_current_user()
 		
@@ -30,11 +35,6 @@ def getUserAndSetupStream(request, response):
 			return u
 	return None
 
-# Some mobile browsers which look like desktop browsers.
-RE_MOBILE = re.compile(r"(iphone|ipod|blackberry|android|palm|windows\s+ce)", re.I)
-RE_DESKTOP = re.compile(r"(windows|linux|os\s+[x9]|solaris|bsd)", re.I)
-RE_BOT = re.compile(r"(spider|crawl|slurp|bot)", re.I)
-        
 def isDesktop(user_agent):
   """
   Anything that looks like a phone isn't a desktop.
@@ -117,8 +117,58 @@ class Stream(webapp2.RequestHandler):
 
 class Admin(webapp2.RequestHandler):
 	def get(self):
-		template_values = {}
+		from application import adminmodel as model
+		import urllib
+		
+		#model.sourceRecount()
+		
+		pageSize = common.getValidatedParam(self.request, self.response, 'pageSize', 20, 'number')
+		offset = common.getValidatedParam(self.request, self.response, 'offset', 0, 'number')
+		filters = common.getValidatedParam(self.request, self.response, 'filters', None, 'dict')
+		order = common.getValidatedParam(self.request, self.response, 'order', None, 'list')
+		
+		logging.info(order)		
+		
+		storyResults = model.getStories(filters, order, pageSize, offset)
+		sourceResults = model.getSources()
+		
+		sources = []
+		for s in sourceResults:
+			if s.storyCount:
+				wps = s.wordCount / s.storyCount
+			else:
+				wps = 'N/A'
+			sources.append({
+				'title': s.title
+				, 'storyCount' : s.storyCount
+				, 'wordCount' : s.wordCount
+				, 'wps': wps
+				, 'urlSafeKey' : s.key.urlsafe()
+				, 'urlSafeTitle':urllib.quote_plus(s.title)
+			})
+
+		stories = []
+		for s in storyResults:
+			stories.append({
+				'title' : s.title
+				, 'creator' : s.creator
+				, 'publication' : s.firstPub.publication
+				, 'wordCount' : s.wordCount
+				, 'urlSafeKey' : s.key.urlsafe()
+			})
+		 
+		
+			 
+		template_values = {'stories':stories, 
+			'sources':sources, 
+			'filterString':common.getParamString(self.request, self.response, 'filters'),
+			'order':order,
+			'orderString':common.getParamString(self.request, self.response, 'order')
+		}
+		logging.info('orderString: {0}'.format(template_values['orderString']))
 		self.response.out.write(template.render(tpath+'admin.html', template_values))
+
+
 
 class BuildRecommendations(webapp2.RequestHandler):
 	def get(self):
@@ -138,44 +188,59 @@ class ResetDataStore(webapp2.RequestHandler):
 		model.clearDataStore('Rec')
 		model.clearDataStore('User')
 		model.clearDataStore('StreamNode')
-		
-class ParseStoryImport(webapp2.RequestHandler):
+		model.clearDataStore('Story')
+
+class AdminPreview(webapp2.RequestHandler):
 	def get(self):
-		gUser = users.get_current_user()
-		if not gUser:
-			self.response.out.write('need to be logged in to do this.')
-			pass
+		urlSafeStoryKey = common.getValidatedParam(self.request, self.response, 'storyKey', None, None)
+		
+		if urlSafeStoryKey:
+			storyKey = model.ndb.Key(urlsafe=urlSafeStoryKey)
+			story = storyKey.get()
+			if story:
+				self.response.out.write("<a href='/admin'>Return to Admin Home</a>&nbsp;&nbsp;")
+				self.response.out.write("<a href='/admin/import?storyKey={0}'>Reimport Story</a>".format(urlSafeStoryKey))
+				self.response.out.write("<br/>")
+				self.response.out.write("<!DOCTYPE html><html><head><meta charset='utf-8'/></head><body style='width:550px;border:1px solid black; padding:25px;'>")			
+				self.response.out.write("Title: {0}<br/> Author: {1}<br/> Publication: {2}<br/> Word Count: {3}<br/>".format(story.title, story.creator[0], story.firstPub.publication, story.wordCount))
+				self.response.out.write("URL: <a target='_blank' href='{0}'>{0}</a><br/>".format(story.firstPub.url))
+				self.response.out.write(story.text)
+				self.response.out.write("</body></html>")
+			else:
+				self.response.out.write('Invalid storyKey provided')
 		else:
-			userId = gUser.user_id()
-			self.response.out.write(userId)
+			self.response.out.write('No storyKey provided')	
+		self.response.out.write("<a href='/admin'>Return to Admin Home</a>")
+			
+
+class ImportStories(webapp2.RequestHandler):
+	def get(self):
+		from application import adminmodel as model
+		model.setup(self.request, self.response)
+		model.setupSources()
+		sourceKey = common.getValidatedParam(self.request, self.response, 'sourceKey', None, None)
+		storyKey = common.getValidatedParam(self.request, self.response, 'storyKey', None, None)
+
+		if storyKey:
+			logging.info('attempting to reimport story')
+			from application import adminmodel as model
+			model.setup(self.request, self.response)
+			model.reimportStory(storyKey)
+			#self.redirect('/admin?m=ImportedStory')
+		elif sourceKey:
+			limit = common.getValidatedParam(self.request, self.response, 'limit', None, 'number')
+			reimport = common.getValidatedParam(self.request, self.response, 'reimport', False, 'bool')
+			offset = common.getValidatedParam(self.request, self.response, 'offset', 0, 'number')
+			model.importStories(sourceKey, limit, offset, reimport)
+		else:
+			self.response.out.write('No storyKey provided. Could not reimport story <br/> requestUri: {0}'.format(self.request.url))
 	
-			from application import parsetogae as p
-			overwrite = self.request.get('o')
-			limit = self.request.get('l')
-			skip = self.request.get('s')
-			importStream = self.request.get('importStream')
-			importStories = self.request.get('importStories')
-			
-			if limit == '':
-				limit = 400
-			if overwrite == '':
-				overwrite = False
-			else: 
-				overwrite = bool(overwrite)
-			if skip == '':
-				skip = 0
-			else:
-				skip = int(skip) 
-			
-			if importStories:
-				p.importStories(self.response, limit, skip, overwrite)
-			else:
-				self.response.out.write('skipping import of stories')				
-			if importStream:				
-				p.importStream(userId, self.response, 400, 0)
-			else:
-				self.response.out.write('skipping import of stream')
-			
+class AdminRecount(webapp2.RequestHandler):
+	def get(self):
+		from application import adminmodel as model
+		model.setup(self.request, self.response)
+		model.sourceRecount()
+	
 class Test(webapp2.RequestHandler):
 	def get(self):
 		pass
@@ -186,9 +251,11 @@ class Test(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', Home),
     ('/reader',Reader),
-	('/parseimport',ParseStoryImport),
-	('/reset',ResetDataStore),
 	('/admin',Admin),
+	('/admin/preview', AdminPreview),
+	('/admin/import',ImportStories),
+	('/admin/reset',ResetDataStore),
+	('/admin/recount',AdminRecount),
 	('/test',Test),
 	('/service/stream',Stream),
 	('/admin/buildrecs',BuildRecommendations)
